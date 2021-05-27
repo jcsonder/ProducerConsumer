1632620c47bf3ebf7793885de180e87042284975
jcson <jcson@DESKTOP-UUEH5R9>
2021-05-27 16:06:26 +0200

    Projektdateien hinzufügen.

---
+++ ProducerConsumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+            var producerTask = Task.Run(() => {
+
+                for (int i = 1; i < 100; i++)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    mediator.Handle(i);
+                }
+            });
+
+            await producerTask;
+        }
+
+        private class Mediator
+        {
+            BlockingCollection<int> events = new BlockingCollection<int>();
+            Task runningCalculation;
+
+            public void Handle(int @event)
+            {
+                System.Console.WriteLine(System.DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff"));
+
+                events.Add(@event);
+
+                if (runningCalculation == null || runningCalculation.IsCompleted)
+                {
+                    runningCalculation = Task.Run(() => SlowBatchProcessing(events));
+                }
+            }
+
+            private Task SlowBatchProcessing(BlockingCollection<int> events)
+            {
+                System.Threading.Thread.Sleep(1000);
+                System.Console.WriteLine($"event calculation: {string.Join(", ", @events)}");
+
+                while (events.Count > 0)
+                {
+                    events.TryTake(out _);
+                }
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
