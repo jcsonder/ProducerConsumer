31ed103325f7524ace30fadf0556402e522299fa
jcsonder <jcsonder@gmail.com>
2021-05-31 05:54:35 +0200

    Cancel with X key

--- ProducerConsumer/Program.cs
+++ ProducerConsumer/Program.cs
@@ -12,31 +12,61 @@ namespace ProducerConsumer
             Mediator mediator = new Mediator();
             var producerTask = Task.Run(() => {
 
-                for (int i = 1; i < 100; i++)
+                for (int i = 1; i <= 100; i++)
                 {
+                    // every 100 ms we have to handle an event
                     System.Threading.Thread.Sleep(100);
                     mediator.Handle(i);
+
+                    bool breakLoop = false;
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey(true);
+                        switch (key.Key)
+                        {
+                            case ConsoleKey.X:
+                                breakLoop = true;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        if (breakLoop)
+                        {
+                            break;
+                        }
+                    }
                 }
             });
 
             await producerTask;
+
+            mediator.Stop();
+            mediator.CalculatedData.ForEach(x => Console.WriteLine($"{x.Item1}_{x.Item2}"));
+
+            Console.ReadLine();
         }
 
         private class Mediator
         {
             private readonly Random _rnd;
             private readonly BlockingCollection<int> _events;
+            
             private Task _runningCalculation;
+            private List<Tuple<int, double>> _calculatedData;
 
             public Mediator()
             {
                 _rnd = new Random();
                 _events = new BlockingCollection<int>();
+                CalculatedData = new List<Tuple<int, double>>();
             }
+
+            public List<Tuple<int, double>> CalculatedData { get => _calculatedData; private set => _calculatedData = value; }
 
             public void Handle(int @event)
             {
-                Console.WriteLine(DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff"));
+                Console.WriteLine($"incoming event: {@event}: {DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff")}");
 
                 _events.Add(@event);
 
@@ -46,17 +76,29 @@ namespace ProducerConsumer
                 }
             }
 
+            public void Stop()
+            {
+                if (_runningCalculation != null)
+                {
+                    // TODO: cancel
+                }
+            }
+
             private Task SlowBatchProcessing(BlockingCollection<int> events)
             {
+                //Console.WriteLine($"SlowBatchProcessing: {string.Join(",", events.ToArray())}");
+
+                // processing takes at least 200ms: Producer creates the events faster then we can process them!
                 int timeout = _rnd.Next(200, 1000);
                 System.Threading.Thread.Sleep(timeout);
                 
-                System.Console.WriteLine($"event calculation: {string.Join(", ", @events)}");
-
-                // remove all items from blocking collection
                 while (events.Count > 0)
                 {
-                    events.TryTake(out _);
+                    if (events.TryTake(out int @event))
+                    {
+                        Console.WriteLine($"                   : {@event}");
+                        CalculatedData.Add(new Tuple<int, double>(@event, @event + 0.1));
+                    }
                 }
 
                 return Task.CompletedTask;
