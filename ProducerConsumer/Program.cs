6a0605c19f9c3a6901718d1f38fcf29bd635541d
jcsonder <jcsonder@gmail.com>
2021-05-31 06:18:14 +0200

    Cancel on X key

--- ProducerConsumer/Program.cs
+++ ProducerConsumer/Program.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProducerConsumer
 {
     class Program
     {
+        static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
         static async Task Main(string[] args)
         {
             Mediator mediator = new Mediator();
             var producerTask = Task.Run(() => {
 
-                for (int i = 1; i <= 100; i++)
+                for (int i = 1; i <= 1000; i++)
                 {
                     // every 100 ms we have to handle an event
-                    System.Threading.Thread.Sleep(100);
+                    Thread.Sleep(100);
                     mediator.Handle(i);
 
                     bool breakLoop = false;
@@ -26,6 +29,7 @@ namespace ProducerConsumer
                         {
                             case ConsoleKey.X:
                                 breakLoop = true;
+                                cancellationTokenSource.Cancel();
                                 break;
                             default:
                                 break;
@@ -37,13 +41,13 @@ namespace ProducerConsumer
                         }
                     }
                 }
-            });
+            }, cancellationTokenSource.Token);
 
             await producerTask;
 
-            mediator.Stop();
             mediator.CalculatedData.ForEach(x => Console.WriteLine($"{x.Item1}_{x.Item2}"));
 
+            Console.WriteLine("End of Main - Press any key to finish");
             Console.ReadLine();
         }
 
@@ -72,33 +76,40 @@ namespace ProducerConsumer
 
                 if (_runningCalculation == null || _runningCalculation.IsCompleted)
                 {
-                    _runningCalculation = Task.Run(() => SlowBatchProcessing(_events));
+                    _runningCalculation = Task.Run(() => SlowBatchProcessingAsync(_events));
                 }
             }
 
-            public void Stop()
-            {
-                if (_runningCalculation != null)
-                {
-                    // TODO: cancel
-                }
-            }
-
-            private Task SlowBatchProcessing(BlockingCollection<int> events)
+            private Task SlowBatchProcessingAsync(BlockingCollection<int> events)
             {
                 //Console.WriteLine($"SlowBatchProcessing: {string.Join(",", events.ToArray())}");
 
                 // processing takes at least 200ms: Producer creates the events faster then we can process them!
-                int timeout = _rnd.Next(200, 1000);
-                System.Threading.Thread.Sleep(timeout);
+                int waitTimeInMs = _rnd.Next(200, 1000);
+                try
+                {
+                    Task.Delay(waitTimeInMs, cancellationTokenSource.Token).Wait(cancellationTokenSource.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    return Task.FromCanceled(cancellationTokenSource.Token);
+                }
                 
                 while (events.Count > 0)
                 {
-                    if (events.TryTake(out int @event))
+                    int @event;
+                    try
                     {
-                        Console.WriteLine($"                   : {@event}");
-                        CalculatedData.Add(new Tuple<int, double>(@event, @event + 0.1));
+                        @event = events.Take(cancellationTokenSource.Token);
                     }
+                    catch (OperationCanceledException)
+                    {
+                        Console.WriteLine("Take operation was canceled. IsCancellationRequested={0}", cancellationTokenSource.IsCancellationRequested);
+                        return Task.FromCanceled(cancellationTokenSource.Token);
+                    }
+                    
+                    Console.WriteLine($"                   : {@event}");
+                    CalculatedData.Add(new Tuple<int, double>(@event, @event + 0.1));
                 }
 
                 return Task.CompletedTask;
